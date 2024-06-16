3adc7fddd2bf675df21b3e9705b9d059f60a199b
cain <gunbos1031@gmail.com>
2024-06-16 17:31:13 +0900

    feat: set condition for create maingoal

--- src/main/kotlin/com/gunb0s/mandalart/model/Mandalart.kt
+++ src/main/kotlin/com/gunb0s/mandalart/model/Mandalart.kt
@@ -7,6 +7,17 @@ data class Mandalart(
     val mainGoals: MutableList<MainGoal> = mutableListOf()
 ) {
     fun addMainGoal(mainGoal: MainGoal) {
+        val row = mainGoal.row
+        val col = mainGoal.col
+
+        if (row == 4 && col == 4) {
+            throw IllegalArgumentException("Invalid row and col: $row, $col")
+        }
+
+        if (!(row in 3..5 && col in 3..5)) {
+            throw IllegalArgumentException("Invalid row and col: $row, $col")
+        }
+
         mainGoals.add(mainGoal)
     }
 }
